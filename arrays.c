3ee3103c14c510b3cbb362ac7e2edc56df2dd2e4
nickpratyaksh <nickpratyaksh@gmail.com>
2023-10-13 00:21:04 +0530

    array searching

--- arrays.c
+++ arrays.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+void print(int arr[], int size);
 int insert(int arr[], int size, int val, int pos);
 int delete(int arr[], int size, int pos);
-
+void LSearch(int arr[], int size, int val);
+void BSearch(int arr[], int size, int val);
 
 void main(){
     int array[15] = {1,2,3,4,5,6,7,8,9,10};
-    delete(array, 10, 5);
+    // delete(array, 10, 5);
+    // LSearch(array, 10, 5);
+    BSearch(array, 10, 5);
 
-    for (int i = 0; i < 13; i++)
-        printf("%d\n",array[i]);
+    // print(array, 10);
+}
+
+void print(int arr[], int size){
+    for(int i = 0; i <= size; i++){
+        printf("%d\n",i);
+    }
 }
 
 int insert(int arr[], int size, int val, int pos){
@@ -25,5 +34,28 @@ int delete(int arr[], int size, int pos){
         arr[i] = arr[i+1];
     }
     return arr[size];
+}
+
+void LSearch(int arr[], int size, int val){
+    for(int i = 0; i <= size; i++){
+        if(arr[i] == val)
+         printf("Item found at %d",i);
+    }
+}
+
+void BSearch(int arr[], int size, int val){
+    int L = 0, U = size;
+    while (L <= U)
+    {
+        int Mid = (L+U)/2;
+        if(arr[Mid] == val){
+        printf("Item found at %d",Mid);
+        return;
+        }
+        if(val > Mid)
+            L = Mid + 1;    
+        else
+            U = Mid - 1;
+    }
     
 }
